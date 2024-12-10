5547d1487fb73bd8355674e4c470c8b04e281699
HARSH <chhedaharsh06.com>
2024-12-10 20:37:18 +0530

    admin dashboard added

--- src/component/Header.js
+++ src/component/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
   };
 
   const cartItemNumber = useSelector((state) => state.product.cartItem);
-  
+
   return (
     <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50 bg-white">
       <div className="flex items-center h-full justify-between">
@@ -38,11 +38,13 @@ const Header = () => {
         <div className="flex items-center gap-4 md:gap-7">
           <nav className="gap-4 md:gap-6 text-base md:text-lg hidden md:flex">
             <Link to={""}>Home</Link>
+
+            {/* Check if the user is an admin by comparing email with the environment variable */}
             {userData.email === process.env.REACT_APP_ADMIN_EMAIL && (
               <Link to={"dashboard"}>Dashboard</Link>
             )}
           </nav>
-          
+
           <div className="text-2xl text-slate-600 relative">
             <Link to={"cart"}>
               <BsCartFill />
@@ -51,7 +53,7 @@ const Header = () => {
               </div>
             </Link>
           </div>
-          
+
           <div className="text-slate-600" onClick={handleShowMenu}>
             <div className="text-3xl cursor-pointer w-8 h-8 rounded-full overflow-hidden drop-shadow-md">
               {userData.image ? (
